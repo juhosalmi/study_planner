a6f0570b9ff720864142678200527b730dcbb131
juhosalmi <juho.salmi@iki.fi>
2013-04-25 08:21:54 +0300

    More Gui

--- gui.py
+++ gui.py
@@ -1,19 +1,49 @@
-
-
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from studyPlan import StudyPlan
 from plannerIO import PlannerIO
 
+class CourseButton(QtGui.QPushButton):
+  
+    def __init__(self, title, parent):
+        super(CourseButton, self).__init__(title, parent)
 
-class Example(QtGui.QWidget):
+    def mousePressEvent(self, e):
+      
+        QtGui.QPushButton.mousePressEvent(self, e)
+        if e.button() == QtCore.Qt.LeftButton:
+            print 'press'
+        elif e.button() == QtCore.Qt.RightButton:
+            print 'right press'
+            
+class MainWindow(QtGui.QMainWindow):
     
     def __init__(self, studyPlan):
-        super(Example, self).__init__()
+        
+        super(MainWindow, self).__init__()
+        
+        self.studyPlan = studyPlan
+        self.statusBar()
+        studyPlanner = StudyPlanner(studyPlan)
+        self.setCentralWidget(studyPlanner)
+        self.setWindowTitle('Study Planner')    
+        self.show()
+        
+    def keyPressEvent(self, e):
+        
+        if e.key() == QtCore.Qt.Key_Escape:
+            self.close()
+        
+    
+
+class StudyPlanner(QtGui.QWidget):
+    
+    def __init__(self, studyPlan):
+        
+        super(StudyPlanner, self).__init__()
         
         self.studyPlan = studyPlan
         self.initUI()
-        self.center()
         
     def initUI(self):
         
@@ -21,6 +51,7 @@ class Example(QtGui.QWidget):
         
         for year in range(0, len(self.studyPlan.schedule)):
             syear = QtGui.QPushButton(str(year+1))
+            syear.clicked.connect(self.buttonClicked)
             grid.addWidget(syear, 0, year*4, 1, 4)
             I = QtGui.QPushButton('I')
             II = QtGui.QPushButton('II')
@@ -32,7 +63,7 @@ class Example(QtGui.QWidget):
             grid.addWidget(IV, 1, year*4+3)
             for courseName, period in self.studyPlan.schedule[year].iteritems():
                 col = year*4+period.begin-1
-                button = QtGui.QPushButton(courseName)
+                button = CourseButton(courseName, self)
                 colspan = period.end - period.begin + 1
                 for row in range(2, grid.rowCount()+1):
                     if row == grid.rowCount():
@@ -41,17 +72,14 @@ class Example(QtGui.QWidget):
                     if grid.itemAtPosition(row, col) == None:
                         grid.addWidget(button, row, col, 1, colspan)
                         break
-        
         self.setLayout(grid) 
-        self.setWindowTitle('Study Planner')    
-        self.show()
         
-    def center(self):
-    
-        qr = self.frameGeometry()
-        cp = QtGui.QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+            
+    def buttonClicked(self):
+      
+        sender = self.sender()
+        print sender.text() + ' was pressed'
+        #self.statusBar().showMessage(sender.text() + ' was pressed')
         
 def main():
     
@@ -59,7 +87,7 @@ def main():
     studyPlan = plannerIO.loadStudyPlan('aut.csv')
     studyPlan.setAvailableCourses(plannerIO.loadCourses('autcourses.csv'))
     app = QtGui.QApplication(sys.argv)
-    ex = Example(studyPlan)
+    ex = MainWindow(studyPlan)
     sys.exit(app.exec_())
 
 
